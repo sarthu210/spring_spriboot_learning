e80b3efca6eb39424dd957e3805280bb236e06f4
sarthu210 <sarthu102@gmail.com>
2024-07-31 15:43:09 +0530

    added custom properties

--- 1-Java_Spring_Overview/myfirstapp/src/main/java/com/sarthak/myfirstapp/rest/MyRestController.java
+++ 1-Java_Spring_Overview/myfirstapp/src/main/java/com/sarthak/myfirstapp/rest/MyRestController.java
@@ -1,10 +1,22 @@
 package com.sarthak.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+    @Value("${first.name}")
+    public String firstName;
+
+    @Value("${last.name}")
+    public String lastName;
+
+    @GetMapping("/full-name")
+    public String fullName(){
+        return "My name is " + firstName + " " + lastName;
+    }
+
     @GetMapping("/")
     public String helloWorld(){
         return "Hello World!";
