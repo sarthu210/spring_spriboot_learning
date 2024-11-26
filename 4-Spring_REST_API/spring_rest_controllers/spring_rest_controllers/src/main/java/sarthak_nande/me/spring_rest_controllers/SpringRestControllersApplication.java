819edd0633d3284e05b1b302124fc55931601d43
sarthu210 <sarthu102@gmail.com>
2024-11-27 00:08:01 +0530

    Spring Rest Controller

---
+++ 4-Spring_REST_API/spring_rest_controllers/spring_rest_controllers/src/main/java/sarthak_nande/me/spring_rest_controllers/SpringRestControllersApplication.java
@@ -0,0 +1,13 @@
+package sarthak_nande.me.spring_rest_controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestControllersApplication.class, args);
+	}
+
+}
---
+++ 4-Spring_REST_API/spring_rest_controllers/spring_rest_controllers/src/test/java/sarthak_nande/me/spring_rest_controllers/SpringRestControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package sarthak_nande.me.spring_rest_controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
