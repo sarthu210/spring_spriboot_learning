94f5c1a65357a0c3250f85687ebb259ce07a54cf
sarthu210 <sarthu102@gmail.com>
2024-12-29 22:12:59 +0530

    Completed Project By Implementing Spring Data REST

--- 4-Spring_REST_API/project-2/project-using-jpa/src/main/java/jpa_project/project_using_jpa/rest/EmployeeController.java
+++ 4-Spring_REST_API/project-2/project-using-jpa/src/main/java/jpa_project/project_using_jpa/rest/EmployeeController.java
@@ -2,9 +2,7 @@ package jpa_project.project_using_jpa.rest;
 
 import jpa_project.project_using_jpa.enitiy.Employee;
 import jpa_project.project_using_jpa.services.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +17,55 @@ public class EmployeeController {
         this.theEmployeeService = theEmployeeService;
     }
 
-    @GetMapping("/employee")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees()
     {
-        List<Employee> theEmployee = theEmployeeService.findAll();
-        return theEmployee;
+        return theEmployeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeesId}")
+    public Employee getElementById(@PathVariable int employeesId) {
+        Employee theEmploye = theEmployeeService.findById(employeesId);
+
+        if(theEmploye == null)
+        {
+            throw new RuntimeException("Employee Id Not Found - " + employeesId);
+        }
+
+        return theEmploye;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee theEmployee)
+    {
+        theEmployee.setId(0);
+
+        Employee dbEmployee = theEmployeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @PostMapping("/employees/{employeesId}")
+    public String removeEmployee(@PathVariable int employeesId)
+    {
+        Employee theEmployee = theEmployeeService.findById(employeesId);
+
+        if(theEmployee == null)
+        {
+            throw new RuntimeException("Employee Id Not Found - " + employeesId);
+        }
+
+        theEmployeeService.deleteById(employeesId);
+
+        return "Deleted " + employeesId + "SuccessFully!";
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee)
+    {
+        Employee dbEmployee = theEmployeeService.save(theEmployee);
+
+
+        return dbEmployee;
     }
 }
---
+++ 4-Spring_REST_API/project-3/project-using-jpa/src/main/java/jpa_project/project_using_jpa/ProjectUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package jpa_project.project_using_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUsingJpaApplication.class, args);
+	}
+
+}
