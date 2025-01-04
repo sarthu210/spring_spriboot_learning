9db2ce4932bb507d3c47784a02d377f74ac7a9ba
sarthu210 <sarthu102@gmail.com>
2025-01-04 23:45:15 +0530

    Role Based Restriction Done For Each Endpoint

--- 5-Spring_REST_API_Security/Basic-Custom-Auth/src/main/java/com/employee/employee_project1/security/AuthSecurityConfig.java
+++ 5-Spring_REST_API_Security/Basic-Custom-Auth/src/main/java/com/employee/employee_project1/security/AuthSecurityConfig.java
@@ -2,9 +2,13 @@ package com.employee.employee_project1.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AuthSecurityConfig{
@@ -15,22 +19,42 @@ public class AuthSecurityConfig{
         UserDetails sarthak = User.builder()
                 .username("Sarthak")
                 .password("{noop}Sarthak@123")
-                .roles("STUDENT")
+                .roles("EMPLOYEE")
                 .build();
 
         UserDetails shraddha = User.builder()
                 .username("Shraddha")
                 .password("{noop}Shraddha@123")
-                .roles("STUDENT","FRIEND","WIFE")
+                .roles("MANAGER")
                 .build();
 
         UserDetails madhav = User.builder()
                 .username("Madhav")
                 .password("{noop}Madhav@123")
-                .roles("STUDENT" , "FRIEND")
+                .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(sarthak,madhav,shraddha);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(configure ->
+                configure
+                        .requestMatchers(HttpMethod.GET , "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE , "api/employees").hasRole("Admin")
+                );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+
     }
 
 }
